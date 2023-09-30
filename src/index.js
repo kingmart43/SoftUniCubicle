613f4fe36b0b0966cfb37e217ae1793027f42ee9
kingmart43 <146567624+kingmart43@users.noreply.github.com>
2023-09-30 20:44:09 +0300

    Add cubeController

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const handlebarsConfig = require('./config/handlebarsConfig');
 const expressConfig = require('./config/expressConfig');
 const homeController = require('./controllers/homeController');
-
+const cubeController = require('./controllers/cubeController')
 const app = express();
 const PORT = 5555;
 
@@ -13,6 +13,7 @@ handlebarsConfig(app);
 
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 
 
